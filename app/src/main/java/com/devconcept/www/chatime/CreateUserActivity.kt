e43b6567f2920d7ed61662e531bbe8d126891fe4
Harry <poripori1030@gmail.com>
2017-11-23 16:30:15 +0800

    CreateUserActivity UI

---
+++ app/src/main/java/com/devconcept/www/chatime/CreateUserActivity.kt
@@ -0,0 +1,27 @@
+package com.devconcept.www.chatime
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColorButtonClicked(view: View) {
+
+    }
+
+    fun createUserButtonClicked(view: View) {
+
+    }
+
+
+}
--- app/src/main/java/com/devconcept/www/chatime/LoginActivity.kt
+++ app/src/main/java/com/devconcept/www/chatime/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.devconcept.www.chatime
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,7 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserButtonClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
     fun loginLoginButtonClicked(view: View) {
