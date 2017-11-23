2933d41a657ad0f83030032eb96bf5cb74c4ede3
Harry <poripori1030@gmail.com>
2017-11-23 16:49:32 +0800

    Generate Avatar And Background Color

--- app/src/main/java/com/devconcept/www/chatime/CreateUserActivity.kt
+++ app/src/main/java/com/devconcept/www/chatime/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.devconcept.www.chatime
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,34 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageVIew.setImageResource(resourceId)
     }
 
     fun generateBackgroundColorButtonClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageVIew.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println(avatarColor)
     }
 
     fun createUserButtonClicked(view: View) {
